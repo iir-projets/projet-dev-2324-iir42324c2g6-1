abc89131b3a921fb478ac062ef23176a784deae1
Matcha <hayatessaadi13@gmail.com>
2024-05-02 20:01:45 +0100

    new commit

--- client/src/component/action/flight-actions.js
+++ client/src/component/action/flight-actions.js
@@ -1,8 +1,8 @@
 import flightsapi from '../api/flightsapi';
-import { SAVE_BOOKING, SEARCH_FLIGHTS } from "./actionTypes"
+import { SAVE_BOOKING,SEARCH_FLIGHTS } from "./actionTypes"
 export const searchFlights = (info) => async (dispatch) => {
     try {
-        const response = await flightsapi.get(`/find-flights/${info.origin}/${info.dest}/${info.date}`)
+        const response = await flightsapi.get(`/find-flights/${info.origin}/${info.dest}`)
      
         if(response && response.status === 200){
              dispatch({type:SEARCH_FLIGHTS, payload : response.data});
@@ -10,7 +10,22 @@ export const searchFlights = (info) => async (dispatch) => {
      
        }catch (error) {
          console.log(error)
-       }
+       }}
+
+
+export const saveBooking = (info) => async (dispatch) => {
+
+        console.log(info);
+        try {
+         const response = await flightsapi.post("/save-booking", {...info})
+      
+         if(response && response.status === 200){
+              dispatch({type:SAVE_BOOKING, payload : response.data});
+         }
+      
+        }catch (error) {
+          console.log(error)
+        }
       
 }
 
